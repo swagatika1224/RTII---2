64921764cf92dbbf74cc473909b0b5fe6dc1d9e3
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-10-18 18:54:01 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ previous_result = results[0].label;
 var synth = window.speechSynthesis;
 speak_data = 'Object detected is - '+results[0].label;
 var utterThis = new SpeechSynthesisUtterance(speak_data);
-
+synth.speak(utterThis);
 document.getElementById("result_object_name").innerHTML = results[0].
 label;
 document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
